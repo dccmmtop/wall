106122a6f6661592aa69ce98fad6fe4d1f3a0b70
dccmmtop <dccmmtop@gmail.com>
2019-03-09 18:48:25 +0800

    编辑message

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
-import { Actions,Scene, Router, Stack } from "react-native-router-flux";
-import MapInfo from "./components/mapinfo";
-import React, { Component } from "react";
-import Login from "./components/login";
-import Regist from "./components/regist";
-import NewMessage from "./components/new_message";
-import ShowMessage from "./components/show_message";
-import { BackHandler } from "react-native";
+import {Actions, Scene, Router, Stack} from 'react-native-router-flux';
+import MapInfo from './components/mapinfo';
+import React, {Component} from 'react';
+import Login from './components/login';
+import Regist from './components/regist';
+import NewMessage from './components/new_message';
+import ShowMessage from './components/show_message';
+import EditMessage from './components/edit_message';
+import {BackHandler} from 'react-native';
 
 export default class App extends Component {
   onBackPress() {
-    if (Actions.currentScene === "mapInfo"){
+    if (Actions.currentScene === 'mapInfo') {
       BackHandler.exitApp();
       return false;
-    }else{
-      Actions.pop()
-      return true
+    } else {
+      Actions.pop();
+      return true;
     }
   }
   render() {
     return (
-      <Router  backAndroidHandler={this.onBackPress} >
-        <Stack key="root" hideNavBar >
-          <Scene key="mapInfo" component={MapInfo}  initial/>
+      <Router backAndroidHandler={this.onBackPress}>
+        <Stack key="root" hideNavBar>
+          <Scene key="mapInfo" component={MapInfo} initial />
           <Scene key="login" component={Login} />
           <Scene key="regist" component={Regist} />
           <Scene key="new_message" component={NewMessage} />
+          <Scene key="edit_message" component={EditMessage} />
           <Scene key="show_message" component={ShowMessage} />
         </Stack>
       </Router>
